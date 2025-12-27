5e9c3440aaa4f5269a606892640f1a79205be502
Dean Howe <hideanhowe@gmail.com>
2025-12-27 12:24:01 +0000

    replaceAll solution

--- index.js
+++ index.js
@@ -5,3 +5,29 @@
 // HTML tags may be nested.
 // Remove the tags and any attributes.
 // For example, '<a href="#">Click here</a>' should return "Click here".
+
+// function stripTags(html) {
+//   console.log(html);
+
+//   // find the input string
+//   const regex = /(>[a-z\s]+<)/;
+//   console.log(regex.test(html));
+//   return html;
+// }
+
+function stripTags(html) {
+  console.log(html);
+
+  // find the input string
+  // const regex = /(>[a-z\s]+<)/
+  // console.log(regex.test(html))
+  // return html;
+
+  // replace all tags
+  const regex = /<[^>]*>/g;
+
+  // swap out tags
+  const plainText = html.replaceAll(regex, '');
+
+  return plainText;
+}
